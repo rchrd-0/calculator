3b4fc11ea435f732800a8f137a60e0914adade78
richard <richard.ytli+f@gmail.com>
2022-01-03 12:12:21 +0800

    Add clear button

--- script.js
+++ script.js
@@ -1,80 +1,28 @@
 const container = document.querySelector('.container');
-const workingDisplay = container.querySelector('#working');
-const runningDisplay = container.querySelector('#running');
+const working = container.querySelector('#working');
+const running = container.querySelector('#running');
 const buttons = container.querySelector('.buttons');
 
-const input = [];
-buttons.addEventListener('click', event => {
-    const target = event.target;
+const clear = container.querySelector('#clear');
+let memory = {value: [], operation: '', result: 0,};
+let input = [];
+let pasta = [];
+const reset = () => {
+    memory = {value: [], operation: '', result: 0,};
+    // memory.value.splice(0, memory.value.length);
+    // memory.operation.splice(0, memory.operation.length);
+    // memory.result = 0
+    input.splice(0, input.length);
+    pasta.splice(0, pasta.length);
+    working.textContent = 0;
+    running.textContent = null;
+}
+clear.addEventListener('click', event => reset())
 
-    if (target.getAttribute('class') === 'number') {
-        if (workingDisplay.textContent == '0' ) workingDisplay.textContent = 
-            target.textContent;
-        else workingDisplay.textContent += target.textContent;
-        console.log(`Number Input:`) 
-        console.table(input)
-    }
+// const displayOperation = document.createElement('div');
+// displayOperation.setAttribute('id', 'display-Operation');
 
-    if (target.getAttribute('class') === 'operator') {
-        //Change operator instead of adding another operator to array
-        if (input.operation && (!workingDisplay.textContent || input.result)) input.operation.pop();
-
-
-        if (input.value && !!workingDisplay.textContent) {
-            store(input, 'value', +workingDisplay.textContent);
-            let result = operate(...[input.operation.pop()], ...input.value);
-            store(input, 'result', result);
-        }
-        
-        if (input.result && input.result.length) {
-            input.value.splice(0, input.value.length, input.result.pop()) 
-        }   else {
-                store(input, 'value', +workingDisplay.textContent);
-        }
-
-        // store(input, 'value', +workingDisplay.textContent);
-
-        runningDisplay.textContent = `${input.value} ${target.textContent}`
-        workingDisplay.textContent = null;
-        console.log(`Operator Input:`) 
-        console.table(input);
-
-    }
-
-    if (target.getAttribute('class') === 'calculate') {
-        
-        // // if (input.value.length < 2 && !workingDisplay.textContent) return
-        store(input, 'value', +workingDisplay.textContent);
-        let result = operate(...input.operation, ...input.value);
-        // runningDisplay.textContent = `${}`
-        runningDisplay.textContent = `${[...runningDisplay.textContent].join('')} ${workingDisplay.textContent} ${target.textContent}`;
-        // // runningDisplay.textContent += ` ${[...input.value]} ${target.textContent}`
-        workingDisplay.textContent = result;
-        // store(input, 'result', result);
-        // console.table(input);
-    }
-
-    // if (target.getAttribute('id') === 'clear') {
-    //     result = null;
-    //     workingDisplay.textContent = null;
-    //     runningDisplay.textContent = null;
-    //     input.splice(0, input.length);
-    // }
-
-});
-
-// function calculate() {
-//     if (workingDisplay.textContent && store.value.length < 2) {
-//         store(input, 'value', +workingDisplay.textContent);
-//         store(input, 'operation', target.getAttribute('id'));
-//         let result = operate(...input.operation, ...input.value);
-//         runningDisplay.textContent += ` ${input.value[1]} ${target.textContent}`
-//         workingDisplay.textContent = result;
-//         console.table(input);
-//     } else return
-// }
-
-const operate = (operator, a, b) => {
+const evaluate = (operator, a, b) => {
     switch (operator) {
         case 'add':
             return a + b;
@@ -87,66 +35,122 @@ const operate = (operator, a, b) => {
     }
 };
 
-const store = (array, key, value) => { 
-    if (!value) return
-    if (!array[key]) array[key] = [value];
-    else array[key].push(value);
-};
-    // if (target.getAttribute('class') === 'operator') {
-    //     store(input, "value", inputValue);
-    //     runningDisplay.textContent += `${inputValue} `
-
-    //     switch (inputOperation) {
-    //         case 'add':
-    //             runningDisplay.textContent += '+ '
-    //             break;
-    //         case 'subtract':
-    //             runningDisplay.textContent += '- '
-    //             break;
-    //         case 'multiply':
-    //             runningDisplay.textContent += 'x '
-    //             break;
-    //         case 'divide':
-    //             runningDisplay.textContent += '÷ '
-    //     }
-
-    //     if (!input.operation) input.operation = inputOperation;
-
-    //     currentDisplay.textContent = null;
-    // }
-
-    // if (target.getAttribute('id') === 'equals') {        
-    //     store(input, "value", inputValue);
-    //     runningDisplay.textContent += `${inputValue}`
-    //     console.table(input);
-    //     console.log(input);
-    //     currentDisplay.textContent = operate(input.operation, input.value[0], input.value[1])
-    // }
-    //     .reduce(output, next) {
+const storeValue = (memoryObj, inputArr) => {
+    if (inputArr.length) {
+        memoryObj.value.push(
+            inputArr.reduce((a, b) => {
+                a += b;
+                return +a;
+            }, 0)
+        )
+        inputArr.splice(0, input.length);
+    } else return
+}
 
 
-    // }
 
-    // if (target.getAttribute('id') === 'clear') display.textContent = null;
-
-
-// let store = (obj, key, value) => {
-//     if (obj[key]) obj[key].push(value)
-//     else if (!obj[key]) obj[key] = [value];
-//     return obj;
-// }
-
-// function store(n, o) {
+buttons.addEventListener('click', event => {
+    const target = event.target;
     
-//     const testObj = {};
-//     if (!testObj.value) {
-//         testObj.value = [n]
-//     }
+    if (target.getAttribute('class') === 'number') {
+        if (working.textContent === '0' && target.textContent === '0') return
+        else if (working.textContent === '0') {
+            working.textContent = target.textContent;
+        } else {
+            working.textContent += target.textContent;
+        }
 
-//     if (testObj.value) {
-//         testObj.value.push(o);
-//     }
+        input.push(target.textContent);
 
-//     testObj
+        // if (memory.value.length === 1) {
+        //     pasta.splice(0, pasta.length, +input.join(''));
+        //     input.splice(0, input.length)
+        //     memory.result = evaluate(...memory.operation, memory.value[0], pasta[0])
+        //     running.textContent = memory.result;
+
+        // }
+        
+    }
+
+    if (target.getAttribute('class') === 'operator') {
+        const displayOperator = target.textContent;
+        storeValue(memory, input);
+
+        memory.operation = `${target.getAttribute('id')}`
+        working.textContent = `${memory.value[0]} ${displayOperator} `
+    }
+
+    if (target.getAttribute('class') === 'equals') {
+        if (memory.value.length <= 1 && !input.length) return
+        storeValue(memory, input);
+        
+        memory.result = evaluate(memory.operation, ...memory.value)
+        running.textContent = memory.result;
+        } else return
+})
+
+
+        // if (!input.length && !!memory.result) {
+        //     running.textContent = null;
+        //     working.textContent = `${memory.result} ${target.textContent}`;
+        //     memory.value.push(memory.result);
+        //     memory.result = 0;
+        // }
+        // if (input.length) {
+        //     memory.value.push(
+        //         input.reduce((start, next) => {
+        //             start += next
+        //             return +start
+        //         }, 0)
+        //     );
+        //     input.splice(0, input.length);
+        // } else return
+
+
+
+        // if (!memory.value.length && !!memory.result) {
+        //     memory.value.push(memory.result)
+        // }
+
+        // working.textContent += `${input[0]}`;
+
+
+
+    // if (target.getAttribute('class') === 'equals') {
+    //     if (!input.length && !!memory.result) return
+    //     storeValue(memory, input);
+    //     memory.result = operate(memory.operation, ...memory.value);
+    //     memory.value.splice(0, 2);
+    //     working.textContent += ' ='
+    //     running.textContent = memory.result;
+
+        // if (input.length) {
+        //     memory.value.push(
+        //         input.reduce((start, next) => {
+        //             start += next
+        //             return +start
+        //         })
+        //     );
+        //     input.splice(0, input.length);
+        // }
+
+    // };
+
+
+
+// let x = (a, b) => {
+//     console.log(a);
+//     console.log(b);
 // }
-// console.log(store(3, 2))
+
+        // input.outgoing.push(input.value.join(''))
+        // memory.value.push(+input.outgoing.splice(0, input.outgoing.length));
+        // console.table(input);
+        // console.table(memory);
+
+
+    // if (target.getAttribute('class') === 'calclulate') {
+    //     memory.value.push(+working.textContent);
+    //     console.table(memory);
+        
+    // }
